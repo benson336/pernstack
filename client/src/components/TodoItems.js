a17167bb7c43c488830c29dda0c6ce616a12fd63
unknown <benson.wen36@gmail.com>
2021-11-06 12:44:54 -0700

    11_6 commit 2

---
+++ client/src/components/TodoItems.js
@@ -0,0 +1,13 @@
+import React, { Fragment, useState } from "react";
+
+const TodoItems = () => {
+
+    return (
+        <Fragment>
+            <h1 class="text-center mt-5">TodoItems</h1>
+        </Fragment>
+        
+    )
+}
+
+export default TodoItems;
